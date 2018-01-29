49732ee1bbaeed0022cbe05277d6affb2006d625
raidlman <raidlman@gmail.com>
2018-01-29 18:54:08 +0100

    added moving ball

--- 1dpong.ino
+++ 1dpong.ino
@@ -17,9 +17,6 @@
 #define PLAYER1_PIN 8
 #define PLAYER2_PIN 9
 
-// Global variables
-CRGB leds[NUM_LEDS];
-
 class Button {
   enum ButtonState {
       BUTTON_RELEASED = 0,
@@ -56,17 +53,121 @@ class Button {
 };
 
 struct Player {
-  Button button;  
+  uint8_t lifes;
+  uint8_t hitbox_min;
+  uint8_t hitbox_max;
+  Button button;
+};
+
+class Ball {
+  private:
+  
+    int8_t position;
+    double speed;
+    int8_t direction;
+    unsigned long time;
+
+    uint8_t distance_to_field_boundary (Player &player) {
+      return abs(player.hitbox_min - position);
+    }
+    
+    // convert speed (in m/s) to timer delay
+    uint16_t speed_to_timer() {
+      double s = speed; // + ball.speedup;
+      return (uint16_t)(STRIPE_LENGTH*1000 / ((NUM_LEDS)*speed));
+    }
+    
+  public:
+    void init(int8_t p, double s, int8_t d) {
+      position = p;
+      speed = s;
+      direction = d;
+    }
+  
+    bool is_inside_hitbox(Player &player) {
+      if ( player.hitbox_min <= position && position <= player.hitbox_max ) {
+        return true;
+      }
+      return false;
+    }
+    
+    bool timer() {
+      if ( millis() - time >= speed_to_timer() ) {
+        //Serial.println(time);
+        //Serial.println(millis() - time);
+        time = millis();
+        return true;
+      }
+      return false;
+    }
+
+    int8_t get_position() {
+      return position;
+    }
+    
+    void set_position(int8_t p) {
+      position = p;
+    }
+
+    int8_t get_direction() {
+      return direction;
+    }
+    /*
+    void advance() {
+      position += direction;
+    }*/
+};
+
+class Screen {
+  private:
+    // Global variables
+    CRGB leds[NUM_LEDS];
+    
+  public:
+  
+    void init() {
+      // set chipset type, color order of LEDs and number of LEDs on stripe
+      FastLED.addLeds<LED_TYPE, LED_COLOR_ORDER>(leds, NUM_LEDS);
+      
+      // set global brightness
+      FastLED.setBrightness( BRIGHTNESS );
+      
+      // turn off all LEDs
+      for (uint8_t i=0; i<NUM_LEDS; i++){
+        leds[i] = CRGB::Black;
+      }
+       
+      FastLED.show();
+    }
+    
+    void advance_ball(Ball &b, Player &p1, Player &p2) {
+      leds[b.get_position()] = CRGB::Black;
+      
+      for (uint8_t i=p1.hitbox_min; i<p1.hitbox_max; i++) {
+        leds[i] = CRGB::Green;
+      }
+      for (uint8_t i=p2.hitbox_min; i<p2.hitbox_max; i++) {
+        leds[i] = CRGB::Blue;
+      }
+      
+      b.set_position(b.get_position() + b.get_direction());
+      leds[b.get_position()] = CRGB::White;
+      FastLED.show();
+    }
 };
 
 Player player_1;
 Player player_2;
 Button restart;
+Screen screen;
+Ball ball;
 
 void setup() {
   player_1.button.set_pin(PLAYER1_PIN);
   player_2.button.set_pin(PLAYER2_PIN);
   restart.set_pin(RESTART_PIN);
+  screen.init();
+  ball.init(0,0.2,1);
 
   Serial.begin( 9600 );
 }
@@ -80,5 +181,9 @@ void loop() {
   }
   if (restart.is_pressed()) {
     Serial.println("Button Restart pressed!");
+  }
+
+  if (ball.timer()) {
+    screen.advance_ball(ball, player_1, player_2);
   }
 }
