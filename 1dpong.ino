e41a16801abfd8d922c5eb8a3b2652a0623eafce
raidlman <raidlman@gmail.com>
2018-01-30 19:40:49 +0100

    extracted drawing score to method

--- 1dpong.ino
+++ 1dpong.ino
@@ -56,6 +56,7 @@ struct Player {
   uint8_t lifes;
   uint8_t hitbox_min;
   uint8_t hitbox_max;
+  CRGB color;
   Button button;
 };
 
@@ -112,6 +113,10 @@ class Ball {
     int8_t get_direction() {
       return direction;
     }
+
+    void reverse_direction() {
+      direction *= -1;
+    }
     /*
     void advance() {
       position += direction;
@@ -121,7 +126,13 @@ class Ball {
 class Screen {
   private:
     // Global variables
-    CRGB leds[NUM_LEDS];
+    CRGB leds[NUM_LEDS-1];
+
+    void draw_player_score(Player p){
+      for (uint8_t i=p.hitbox_min; i<=p.hitbox_max; i++) {
+        leds[i] = p.color;
+      }
+    }
     
   public:
   
@@ -143,12 +154,8 @@ class Screen {
     void advance_ball(Ball &b, Player &p1, Player &p2) {
       leds[b.get_position()] = CRGB::Black;
       
-      for (uint8_t i=p1.hitbox_min; i<=p1.hitbox_max; i++) {
-        leds[i] = CRGB::Green;
-      }
-      for (uint8_t i=p2.hitbox_min; i<=p2.hitbox_max; i++) {
-        leds[i] = CRGB::Blue;
-      }
+      draw_player_score(p1);
+      draw_player_score(p2);
       
       b.set_position(b.get_position() + b.get_direction());
       leds[b.get_position()] = CRGB::White;
@@ -169,14 +176,17 @@ void setup() {
   player_1.hitbox_max=7;
   player_2.hitbox_min=NUM_LEDS-9;
   player_2.hitbox_max=NUM_LEDS-1;
+  player_1.color = CRGB::Green;
+  player_2.color = CRGB::Blue;
   restart.set_pin(RESTART_PIN);
   screen.init();
   ball.init(0,0.2,1);
 
-  Serial.begin( 9600 );
+//  Serial.begin( 9600 );
 }
 
 void loop() {
+/*
   if (player_1.button.is_pressed()) {
     Serial.println("Button P1 pressed!");
   }
@@ -186,7 +196,7 @@ void loop() {
   if (restart.is_pressed()) {
     Serial.println("Button Restart pressed!");
   }
-
+*/
   if (ball.timer()) {
     screen.advance_ball(ball, player_1, player_2);
   }
