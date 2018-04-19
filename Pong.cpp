8b342ecb910ecb5d39b2697f4d7197128ffbc908
raidlman <raidlman@gmail.com>
2018-04-19 11:19:51 +0200

    licensed project under GPLv3

--- Pong.cpp
+++ Pong.cpp
@@ -1,3 +1,22 @@
+/*
+ * 1D Pong - Pong-like game run on an Arduino MCU
+ * Copyright (C) 2018  Stephan Riedel - raidlman@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Pong.cpp
 #include "Pong.h"
 
 Pong::Pong(uint8_t player_1_pin, uint8_t player_2_pin, uint8_t lifes, uint16_t button_lock_time,
