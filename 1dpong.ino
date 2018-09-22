ddcdbd2f6bf2a3a72996c599cd18f493e4bdac22
Stefan Freitag <stefan.fr@gmx.de>
2018-09-22 23:03:10 +0200

    - altered parameters to raidlman repo default ones in order to being able to submit merge requests later on

--- 1dpong.ino
+++ 1dpong.ino
@@ -25,19 +25,19 @@ const uint16_t BUTTON_LOCK_TIME = 1000;
 const uint16_t RESTART_LOCK_TIME = 1000;
 
 // FastLED settings
-const uint8_t NUM_LEDS = 100;
-const double STRIPE_LENGTH = 1.66;
+const uint8_t NUM_LEDS = 60;
+const double STRIPE_LENGTH = 1.0;
 //const EOrder LED_COLOR_ORDER = BGR;
 //const ESPIChipsets LED_TYPE = APA102;
 const uint8_t BRIGHTNESS = 64; //max. 255
 
 // Digital pins settings
-const uint8_t RESTART_PIN = 2;
-const uint8_t PLAYER1_PIN = 3;
-const uint8_t PLAYER2_PIN = 4;
+const uint8_t RESTART_PIN = 7;
+const uint8_t PLAYER1_PIN = 8;
+const uint8_t PLAYER2_PIN = 9;
 
 // Random settings
-const uint8_t RANDOM_SEED_PIN = 4;
+const uint8_t RANDOM_SEED_PIN = 6;
 
 Pong pong(PLAYER1_PIN, PLAYER2_PIN, LIFES, BUTTON_LOCK_TIME,
   NUM_LEDS, STRIPE_LENGTH, BRIGHTNESS,
